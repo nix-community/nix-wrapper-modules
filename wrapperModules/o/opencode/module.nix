1db3c116a6aa61823f8d8f3c47c306846428fc54
LodWKobku <101132529+LodWKobku@users.noreply.github.com>
2026-09-09 21:12:21 +0200

    feat(wrapperModules.opencode): add option for configurating tui settings (#618)

--- wrapperModules/o/opencode/module.nix
+++ wrapperModules/o/opencode/module.nix
@@ -7,18 +7,37 @@
 }:
 {
   imports = [ wlib.modules.default ];
-  options.settings = lib.mkOption {
-    type = wlib.types.structuredValueWith { typeName = "JSON"; };
-    default = { };
-    description = "Sets OPENCODE_CONFIG for github:sst/opencode";
+  options = {
+    settings = lib.mkOption {
+      type = wlib.types.structuredValueWith { typeName = "JSON"; };
+      default = { };
+      description = "Sets OPENCODE_CONFIG for github:sst/opencode";
+    };
+    tui-settings = lib.mkOption {
+      type = wlib.types.structuredValueWith { typeName = "JSON"; };
+      default = { };
+      description = "Sets OPENCODE_TUI_CONFIG for github:sst/opencode";
+    };
   };
   config = {
-    meta.maintainers = [ wlib.maintainers.birdee ];
+    meta.maintainers = [
+      wlib.maintainers.birdee
+      wlib.maintainers.lodwkobku
+    ];
     package = lib.mkDefault pkgs.opencode;
-    envDefault.OPENCODE_CONFIG = config.constructFiles.generatedConfig.path;
-    constructFiles.generatedConfig = {
-      relPath = "${config.binName}-config.json";
-      content = builtins.toJSON config.settings;
+    envDefault = {
+      OPENCODE_CONFIG = config.constructFiles.opencodeConfig.path;
+      OPENCODE_TUI_CONFIG = config.constructFiles.opencodeTuiConfig.path;
+    };
+    constructFiles = {
+      opencodeConfig = {
+        relPath = "${config.binName}-config.json";
+        content = builtins.toJSON config.settings;
+      };
+      opencodeTuiConfig = {
+        relPath = "${config.binName}-tui-config.json";
+        content = builtins.toJSON config.tui-settings;
+      };
     };
   };
 }
